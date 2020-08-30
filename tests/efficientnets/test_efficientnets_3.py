64683dbafdc111f474bd80fcc4bed44766db9c07
ydcjeff <ydcjeff@outlook.com>
2020-08-31 01:23:41 +0630

    test: gh action test

---
+++ tests/efficientnets/test_efficientnets_3.py
@@ -0,0 +1,33 @@
+import os
+
+import pytest
+from hydra.experimental import compose, initialize
+from src.efficientnets.run import main as efficientnets_main
+
+
+@pytest.mark.parametrize(
+    ["name", "test"],
+    [
+        pytest.param("efficientnet-b2", True),
+        pytest.param("efficientnet-b2", False),
+        pytest.param("efficientnet-b5", False),
+        pytest.param("efficientnet-b5", True),
+    ],
+)
+def test_efficientnets(name, test):
+    with initialize(config_path=os.getcwd() + "/conf"):
+        cfg = compose(
+            config_name="efficientnets",
+            overrides=[
+                f"name={name}",
+                "logger=false",
+                f"test={test}",
+                "ckpt=false",
+                "pl.max_epochs=1",
+                "pl.gpus=0",
+                "pl.limit_train_batches=1",
+                "pl.limit_val_batches=1",
+                "pl.limit_test_batches=1",
+            ],
+        )
+        efficientnets_main(cfg)
