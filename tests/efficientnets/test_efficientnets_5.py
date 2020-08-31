a907f474e20f59c3b24247d9867efb6de933917c
ydcjeff <ydcjeff@outlook.com>
2020-08-31 16:10:05 +0630

    test: fix gh action

--- tests/efficientnets/test_efficientnets_5.py
+++ tests/efficientnets/test_efficientnets_5.py
@@ -7,13 +7,13 @@ from model.efficientnets.run import main as efficientnets_main
 
 
 @pytest.mark.parametrize(
-    ["name", "dm"],
+    ["name", "dm", "num_classes"],
     [
-        pytest.param("efficientnet-b5", "cifar10"),
-        pytest.param("efficientnet-b5", "cifar100"),
+        pytest.param("efficientnet-b5", "cifar10", 10),
+        pytest.param("efficientnet-b5", "cifar100", 100),
     ],
 )
-def test_efficientnets(name, dm):
+def test_efficientnets(name, dm, num_classes):
     with initialize_config_dir(os.getcwd() + "/conf"):
         cfg = compose(
             config_name="efficientnets",
@@ -21,7 +21,9 @@ def test_efficientnets(name, dm):
                 f"name={name}",
                 f"dm={dm}",
                 "logger=false",
+                f"lm.num_classes={num_classes}",
                 "pl.max_epochs=1",
+                "pl.fast_dev_run=true",
                 "pl.gpus=0",
                 "pl.limit_train_batches=1",
                 "pl.limit_val_batches=1",
