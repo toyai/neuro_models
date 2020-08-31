2304f36502cadc27a8996b43f831636b180e597a
ydcjeff <ydcjeff@outlook.com>
2020-08-31 15:57:09 +0630

    test: fix gh action

--- tests/efficientnets/test_efficientnets_3.py
+++ tests/efficientnets/test_efficientnets_3.py
@@ -7,23 +7,32 @@ from model.efficientnets.run import main as efficientnets_main
 
 
 @pytest.mark.parametrize(
-    ["name"],
+    ["name", "dm"],
     [
-        pytest.param("efficientnet-b3"),
+        pytest.param("efficientnet-b3", "cifar10"),
+        pytest.param("efficientnet-b3", "cifar100"),
     ],
 )
-def test_efficientnets(name):
+def test_efficientnets(name, dm):
     with initialize_config_dir(os.getcwd() + "/conf"):
         cfg = compose(
             config_name="efficientnets",
             overrides=[
                 f"name={name}",
+                f"dm={dm}",
                 "logger=false",
                 "pl.max_epochs=1",
                 "pl.gpus=0",
                 "pl.limit_train_batches=1",
                 "pl.limit_val_batches=1",
                 "pl.limit_test_batches=1",
+                "dm.train_dataloader_conf.batch_size=1",
+                "dm.train_dataloader_conf.pin_memory=false",
+                "dm.train_dataloader_conf.shuffle=false",
+                "dm.train_dataloader_conf.num_workers=1",
+                "dm.val_dataloader_conf.batch_size=1",
+                "dm.val_dataloader_conf.pin_memory=false",
+                "dm.val_dataloader_conf.num_workers=1",
             ],
         )
         efficientnets_main(cfg)
