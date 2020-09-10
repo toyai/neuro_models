509d9fef141759022eb9f31055b40ed8bfdbd541
DavianYang <kothantyarzarhein@gmail.com>
2020-09-10 20:34:18 +0630

    [Add] DenseNets

--- scripts/densenets/run.py
+++ scripts/densenets/run.py
@@ -23,7 +23,7 @@ def main(cfg: DictConfig = None):
     gym = DenseNetLightning(network, cfg)
     transforms = T.Compose(
         [
-            T.Resize(size=(img_size, img_size)),
+            T.Resize(size=(224, 224)),
             T.ToTensor(),
             T.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
         ]
